2a96738383069c5de117e1614ea3121e781374a1
Richard Blanchette <richardphi1618@gmail.com>
2022-12-05 19:27:17 -0500

    feat(day2): init built tests need to write functions now

---
+++ 50 days of code challenges/day2/main.py
@@ -0,0 +1,6 @@
+def convert_add(input: list[str]) -> int:
+    return 0
+
+
+def check_duplicates(input: list[str]) -> str:
+    return "foo"
---
+++ 50 days of code challenges/day2/test_day2_main.py
@@ -0,0 +1,17 @@
+from main import check_duplicates, convert_add
+
+
+def test_convert_add() -> None:
+    #  [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and summed to 9.
+    test_list = ['1', '3', '5']
+    assert convert_add(test_list) == 9
+
+
+def test_duplicates() -> None:
+    # the list of fruits below should return apple as a duplicate and list of names should return "no duplicates".
+
+    fruits = ['apple', 'orange', 'banana', 'apple']
+    names = ['Yoda', 'Moses', 'Joshua', 'Mark']
+
+    assert check_duplicates(fruits) == "apple"
+    assert check_duplicates(names) == "no duplicates"
