c1447964edd7ed7d0bba4893c971bdabadd78f5f
Richard Blanchette <richardphi1618@gmail.com>
2022-12-08 12:09:20 -0500

    refactor(day5): removed a line that was unneccesary

--- 50 days of code challenges/day5/day5.py
+++ 50 days of code challenges/day5/day5.py
@@ -5,9 +5,8 @@ def my_discount(price: float, percent_discount: float) -> float:
 
 def gender_count(students: list[str]) -> list[tuple]:
     case_checked_students = list(map(lambda x: x.lower(), students))
-    final_count_dict = {i: case_checked_students.count(i) for i in case_checked_students}
-    XY_count = ('Male', final_count_dict["male"])
-    XX_count = ('female', final_count_dict["female"])
+    XY_count = ('Male', case_checked_students.count("male"))
+    XX_count = ('female', case_checked_students.count("female"))
     return [XY_count, XX_count]
 
 
