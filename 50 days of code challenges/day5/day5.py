76f4bc91f9cc16896ff1c4b40a8bff0fe3507804
Richard Blanchette <richardphi1618@gmail.com>
2022-12-08 11:06:27 -0500

    style(flake8): added .flake8 config and some style changes on a couple files

--- 50 days of code challenges/day5/day5.py
+++ 50 days of code challenges/day5/day5.py
@@ -1,14 +1,15 @@
 
 def my_discount(price: float, percent_discount: float) -> float:
-    return price-((percent_discount/100)*price)
+    return price - ((percent_discount / 100) * price)
 
 
-def gender_count(students: list[str]):
-    case_checked_students = list(map(lambda x:x.lower(), students))
+def gender_count(students: list[str]) -> list[tuple]:
+    case_checked_students = list(map(lambda x: x.lower(), students))
     final_count_dict = {i: case_checked_students.count(i) for i in case_checked_students}
     XY_count = ('Male', final_count_dict["male"])
     XX_count = ('female', final_count_dict["female"])
     return [XY_count, XX_count]
+
 
 if __name__ == "__main__":
     price = float(input("What is the price? \n"))
