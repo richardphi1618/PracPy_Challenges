a83d18e4c02c4e7efc74d18df21fdab91f714e2b
Richard Blanchette <richardphi1618@gmail.com>
2022-12-09 10:32:48 -0500

    feat(day-6): found a second way to solve the bonus challenge and wanted to include it

--- 50 days of code challenges/day6/test_day6.py
+++ 50 days of code challenges/day6/test_day6.py
@@ -1,4 +1,4 @@
-from day6 import user_name, zeroed_code
+from day6 import user_name, zeroed_code, zeroed_code_2nd_method
 
 
 def test_user_name() -> None:
@@ -7,3 +7,7 @@ def test_user_name() -> None:
 
 def test_zeroed_code() -> None:
     assert zeroed_code([2, 5, 7, 8, 9]) == [0, 5, 7, 8, 0]
+
+
+def test_zeroed_code_2nd_method() -> None:
+    assert zeroed_code_2nd_method([2, 5, 7, 8, 9]) == [0, 5, 7, 8, 0]
