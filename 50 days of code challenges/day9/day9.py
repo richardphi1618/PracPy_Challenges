8f5562a9e1475e65210a956eba2a27e9d1132aed
Richard Blanchette <richardphi1618@gmail.com>
2022-12-12 11:16:20 -0500

    feat(day-9): added functions and tests for day 9

--- 50 days of code challenges/day9/day9.py
+++ 50 days of code challenges/day9/day9.py
@@ -1,2 +1,18 @@
 
-def 
+def biggest_odd(input: str) -> int:
+    odds = []
+    for i in input:
+        if int(i) % 2 != 0:
+            odds.append(int(i))
+    return max(odds)
+
+
+def zeros_last(input: list[int]) -> list[int]:
+    zeros_cnt = input.count(0)
+    if zeros_cnt != 0:
+        for i in range(zeros_cnt):
+            input.append(input.pop(input.index(0)))
+    else:
+        input.sort()
+
+    return input
--- 50 days of code challenges/day9/test_day9.py
+++ 50 days of code challenges/day9/test_day9.py
@@ -1,1 +1,13 @@
-from day9 import 
+from day9 import biggest_odd, zeros_last
+
+
+def test_biggest_odd() -> None:
+    assert biggest_odd('23569') == 9
+
+
+def test_zeros_last_1st_example() -> None:
+    assert zeros_last([1, 4, 6, 0, 7, 0, 9]) == [1, 4, 6, 7, 9, 0, 0]
+
+
+def test_zeros_last_2nd_example() -> None:
+    assert zeros_last([2, 1, 4, 7, 6]) == [1, 2, 4, 6, 7]
