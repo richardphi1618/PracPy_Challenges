d6a744be5d1b3fe2beaf751df62ae522d71d08fc
Richard Blanchette <richardphi1618@gmail.com>
2022-12-14 18:50:05 -0500

    feat(day-10): refactored functions to make a little cleaner

--- 50 days of code challenges/day10/day10.py
+++ 50 days of code challenges/day10/day10.py
@@ -1,12 +1,10 @@
 
 def hide_password(password: str) -> tuple[str, int]:
-    password_length = len(password)
-    secure_string = '*' * password_length
-    return (secure_string, password_length)
+    return ('*' * len(password), len(password))
 
 
 def convert_numbers(input: list[int]) -> list[str]:
     output = []
-    for idx, i in enumerate(input):
+    for i in input:
         output.append('{:,}'.format(i))
     return output
