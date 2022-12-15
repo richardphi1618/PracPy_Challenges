49f9ec5f01c666771208d8fc6c16ff1001ed001b
Richard Blanchette <richardphi1618@gmail.com>
2022-12-15 16:41:35 -0500

    style(day-6): missed some white spaces and some minor issues

--- 50 days of code challenges/day06/day6.py
+++ 50 days of code challenges/day06/day6.py
@@ -7,9 +7,11 @@ def zeroed_code(input: list[int]) -> list[int]:
     input[-1] = 0
     return input
 
+
 def zeroed_code_2nd_method(input: list[int]) -> list[int]:
     input[::len(input)-1] = [0, 0]
     return input
+
 
 if __name__ == "__main__":
     email = str(input("What is email \n"))
