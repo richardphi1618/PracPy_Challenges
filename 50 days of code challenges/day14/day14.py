a554ce1c3453bd8d0214471f563204b1c1ae801f
richard blanchette <richardphi1618@gmail.com>
2022-12-17 09:25:35 -0500

    refactor(day-14): couple line tweaks and a new test to double check multiple lists

--- 50 days of code challenges/day14/day14.py
+++ 50 days of code challenges/day14/day14.py
@@ -6,14 +6,15 @@ def flat_list(input_list: "list[list]") -> list:
 def your_salary(name:str, periods: int, rate: int = 20)-> str:
     if periods > 100:
         overtime_earned = periods - 100
-        pass
     else:
         overtime_earned = 0
 
     overtime_rate = rate * 1.25
     pay = (periods - overtime_earned) * rate + overtime_earned * overtime_rate
 
-    output = f"Teacher: {name}, Periods: {periods}\nGross salary:{'{:,}'.format(int(pay))}"
+    output = f"Teacher: {name}, "
+    output += f"Periods: {periods}\n"
+    output += f"Gross salary:{'{:,}'.format(int(pay))}"
     return output
 
 if __name__ == "__main__":
--- 50 days of code challenges/day14/test_day14.py
+++ 50 days of code challenges/day14/test_day14.py
@@ -1,9 +1,11 @@
 from day14 import flat_list, your_salary
 
 
-def test_flat_list() -> None:
+def test_flat_list_example() -> None:
     assert flat_list([[2, 4, 5, 6]]) == [2, 4, 5, 6]
 
+def test_flat_list_multiple_lists() -> None:
+    assert flat_list([[2, 4, 5, 6], [2, 6], [2, 4]]) == [2, 4, 5, 6, 2, 6, 2, 4]
 
 def test_your_salary() -> None:
     output = "Teacher: John Kelly, Periods: 105\nGross salary:2,125"
